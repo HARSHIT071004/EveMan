f3fdf037fc52e48174f1a061e188d769092b6359
harshit sharma <harshsharmavatasa@gmail.com>
2025-04-02 13:05:15 -0700

    Setup

--- frontend/src/components/ContactSupport.jsx
+++ frontend/src/components/ContactSupport.jsx
@@ -1,10 +1,10 @@
 import React from "react";
-import { Navigate } from "react-router-dom";
+import { Link, Navigate } from "react-router-dom";
 const ContactSupport = () => {
   return (
     <div className="py-1 px-2 p-2 text-center">
-      <h2 className="text-5xl font-semibold mb-4 text-[rgb(255,255,255)]">Contact <span className="text-purple-400">& Support</span></h2><br/><br/>
-      
+      <h2 className="text-5xl font-semibold mb-4 text-[rgb(255,255,255)]">Contact <span className="text-purple-400">& Support</span></h2><br /><br />
+
       <div className="flex flex-wrap justify-center gap-4">
         {/* <a href="/call" className="px-1 py-1 text-1xl text-black   bg-white rounded-lg  hover:bg-[#000000c4] hover:text-amber-100 transition">
            Call Support
@@ -30,31 +30,31 @@ const ContactSupport = () => {
            Help Center
         </a> */}
 
-<a href="/call" className="px-3 py-2 text-lg text-black  bg-white rounded-lg hover:bg-[#000000c4] hover:text-amber-100 transition">
-  Call Support
-</a>
+        <Link to ={"/call"} className="px-3 py-2 text-lg text-black  bg-white rounded-lg hover:bg-[#000000c4] hover:text-amber-100 transition">
+          Call Support
+        </Link>
 
-<a href="/email" className="px-4 py-2 text-lg text-black bg-white rounded-lg shadow-lg hover:bg-[#000000c4] hover:text-amber-100 transition">
-  Email Support
-</a>
+        <a href="/email" className="px-4 py-2 text-lg text-black bg-white rounded-lg shadow-lg hover:bg-[#000000c4] hover:text-amber-100 transition">
+          Email Support
+        </a>
 
-<a href="/helpcenter" className="px-4 py-2 text-lg text-black bg-white rounded-lg shadow-lg hover:bg-[#000000c4] hover:text-amber-100 transition">
-  Live Chat
-</a>
+        <a href="/helpcenter" className="px-4 py-2 text-lg text-black bg-white rounded-lg shadow-lg hover:bg-[#000000c4] hover:text-amber-100 transition">
+          Live Chat
+        </a>
 
-<a href="/faqs" className="px-4 py-2 text-lg text-black rounded-lg bg-white shadow-lg hover:bg-[#000000c4] hover:text-amber-100 transition">
-  FAQs
-</a>
+        <a href="/faqs" className="px-4 py-2 text-lg text-black rounded-lg bg-white shadow-lg hover:bg-[#000000c4] hover:text-amber-100 transition">
+          FAQs
+        </a>
 
         <div className="flex gap-4">
           <a href="https://instagram.com/sharmaaa" target="_blank" className="text-1xl text-pink-500 hover:text-pink-600 transition">
             Instagram
           </a>
           <a href="https://twitter.com" target="_blank" className="text-1xl text-blue-400 hover:text-blue-500 transition">
-             Twitter
+            Twitter
           </a>
           <a href="https://wa.me/918384030533" target="_blank" className="text-1 xl text-green-500 hover:text-green-600 transition">
-             WhatsApp
+            WhatsApp
           </a>
         </div>
       </div>
--- frontend/src/components/call.jsx
+++ frontend/src/components/call.jsx
@@ -1,33 +1,3 @@
-// import React from "react";
-
-// const Call = () => {
-//   const phoneNumber = "8384030544";
-
-//   return (
-//     <div className="min-h-screen flex items-center justify-center bg-gradient-to-r text-white">
-//       <div className="bg-[rgb(6,6,7)] text-white p-6 rounded-2xl shadow-xl text-center w-80">
-//         <h1 className="text-2xl font-bold mb-2">Need Assistance?</h1>
-//         <p className="text-gray-300">Call us anytime, we are here to help!</p>
-//         <div className="mt-4">
-//           <a
-//             href={`tel:${phoneNumber}`}
-//             className="bg-blue-600 text-white flex items-center justify-center px-4 py-2 rounded-full text-lg font-semibold hover:bg-blue-700 transition duration-300"
-//           >
-//              Call Now: {phoneNumber}
-//           </a>
-//         </div>
-//       </div>
-//     </div>
-//   );
-// };
-
-// export default Call;
-
-
-
-
-
-
 import React from "react";
 
 const Call = () => {
