caf5d69fc13aee0d9af4b8a679e3b6cea2a2f115
harshit sharma <harshsharmavatasa@gmail.com>
2025-04-02 12:04:23 -0700

    deploy error

--- frontend/src/utils/axios.jsx
+++ frontend/src/utils/axios.jsx
@@ -3,7 +3,7 @@ import axios from 'axios'
 const urlAccess = import.meta.env.VITE_DEV;
 
 const instance = axios.create({
-    baseUrl: urlAccess == "production" ? "" : "http://localhost:5000"
+    baseUrl: urlAccess == "production" ? "https://evemanbackend.vercel.app/" : "http://localhost:5000"
 })
 
 export default instance;
