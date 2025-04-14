da439d3df25fef2302fde4eab93ad20d60945ac1
harshit sharma <harshsharmavatasa@gmail.com>
2025-04-14 05:17:15 -0700

    don

--- frontend/src/components/ClientForm.jsx
+++ frontend/src/components/ClientForm.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
-
+import axios from "../utils/axios";
 const ClientForm = () => {
   const [formData, setFormData] = useState({
     budget: "",
