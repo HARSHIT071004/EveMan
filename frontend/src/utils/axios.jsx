731d0a2dc1d4c1da8da2f833352fc61b12f0144f
harshit sharma <harshsharmavatasa@gmail.com>
2025-04-14 05:01:58 -0700

    testing

--- frontend/src/utils/axios.jsx
+++ frontend/src/utils/axios.jsx
@@ -2,7 +2,7 @@ import axios from 'axios'
 
 
 const instance = axios.create({
-    baseURL: "https://evemanbackend.vercel.app/",
+    baseURL: "https://evemanbackend.vercel.app",
     headers: {
         'Authorization': `Bearer ${localStorage.getItem('token')}`
     }
