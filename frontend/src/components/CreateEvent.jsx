2d722b88eef0706c0043369fec9c508ad8a773ac
harshit sharma <harshsharmavatasa@gmail.com>
2025-04-14 05:11:14 -0700

    hello

--- frontend/src/components/CreateEvent.jsx
+++ frontend/src/components/CreateEvent.jsx
@@ -100,6 +100,7 @@
 
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
+import axios from "../utils/axios";
 
 const CreateEvent = () => {
   const [name, setName] = useState("");
